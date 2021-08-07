df1249da590089bb70b2e59579d791b971cc0e95
ItoKobi <ito.kobilol@gmail.com>
2021-08-06 21:01:22 -0400

    Continued working on the dashboard, implemented table

--- frontend/src/components/MainDashboard.vue
+++ frontend/src/components/MainDashboard.vue
@@ -1,9 +1,23 @@
 <template>
-  <section>
-    <div class="flex flex-wrap justify-center items-center mt-4">
-      <div class="w-72 h-40 rounded-lg shadow-lg bg-black m-1"></div>
-      <div class="w-72 h-40 rounded-lg shadow-lg bg-black m-1"></div>
-      <div class="w-72 h-40 rounded-lg shadow-lg bg-black m-1"></div>
+  <section class="max-w-6xl mx-auto">
+    <div class="flex flex-wrap justify-between items-center">
+      <div class="w-full mx-4 my-2 lg:w-80 h-36 lg:h-40 rounded-lg shadow bg-white flex items-center flex-col justify-center">
+        <h3 class="font-bold text-4xl">$500</h3>
+        <span class="uppercase font-bold text-gray-500">all time income</span>
+      </div>
+      <div class="w-full mx-4 my-2 lg:w-80 h-36 lg:h-40 rounded-lg shadow bg-white flex items-center flex-col justify-center">
+        <h3 class="font-bold text-4xl">$100</h3>
+        <span class="uppercase font-bold text-gray-500">this months income</span>
+      </div>
+      <div class="w-full mx-4 my-2 lg:w-80 h-36 lg:h-40 rounded-lg shadow bg-white flex items-center flex-col justify-center">
+        <h3 class="font-bold text-4xl">2</h3>
+        <span class="uppercase font-bold text-gray-500">this months order</span>
+      </div>
+    </div>
+    <div class="mx-4 my-2">
+      <div class="bg-white w-full h-full rounded-lg shadow px-4 py-2">
+        henlo
+      </div>
     </div>
   </section>
 </template>
--- frontend/src/views/Dash/Home.vue
+++ frontend/src/views/Dash/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <section class="max-w-full lg:flex-row flex-col flex max-h-screen overflow-hidden">
-    <div class="lg:w-60 bg-black h-screen text-white flex flex-col p-4">
-      <h1 class="text-2xl font-bold">Pronto</h1>
+    <div class="lg:w-60 bg-black h-screen text-white flex flex-col p-4 select-none">
+      <router-link to="/" class="text-2xl font-bold">Pronto</router-link>
       <br class="lg:block hidden" />
       <br class="lg:block hidden" />
-      <h3 class="border-b my-2 hover:border-purple-600 duration-300 cursor-pointer text-lg">Home</h3>
-      <h3 class="border-b my-2 hover:border-purple-600 duration-300 cursor-pointer text-lg">Orders</h3>
-      <h3 class="border-b my-2 hover:border-purple-600 duration-300 cursor-pointer text-lg">Account</h3>
-      <h3 class="border-b my-2 hover:border-purple-600 duration-300 cursor-pointer text-lg">Products</h3>
+      <h3 v-on:click="currentPanel = 'Main'" :class="currentPanel == 'Main' ? 'border-red-600' : ''" class="font-bold border-b-2 my-2 hover:border-purple-600 duration-300 cursor-pointer text-lg"><i class="fas fa-home"></i> Home</h3>
+      <h3 v-on:click="currentPanel = 'Orders'" :class="currentPanel == 'Orders' ? 'border-red-600' : ''" class="font-bold border-b-2 my-2 hover:border-purple-600 duration-300 cursor-pointer text-lg"><i class="fas fa-folder"></i> Orders</h3>
+      <h3 v-on:click="currentPanel = 'Account'" :class="currentPanel == 'Account' ? 'border-red-600' : ''" class="font-bold border-b-2 my-2 hover:border-purple-600 duration-300 cursor-pointer text-lg"><i class="fas fa-user"></i> Account</h3>
+      <h3 v-on:click="currentPanel = 'Products'" :class="currentPanel == 'Products' ? 'border-red-600' : ''" class="font-bold border-b-2 my-2 hover:border-purple-600 duration-300 cursor-pointer text-lg"><i class="fas fa-box"></i> Products</h3>
     </div>
-    <div class="w-full overflow-y-scroll">
+    <div class="w-full overflow-y-scroll p-10 bg-gray-100 min-h-screen">
       <main-dashboard v-if="currentPanel == 'Main'" />
-      <div v-else>OWO</div>
+      <order-dashboard v-if="currentPanel == 'Orders'" />
     </div>
   </section>
 </template>
@@ -20,6 +20,7 @@
 import {defineComponent} from 'vue';
 
 import MainDashboard from '../../components/MainDashboard.vue';
+import OrderDashboard from '../../components/OrderDashboard.vue';
 
 export default defineComponent({
   name: "DashboardMain",
@@ -29,7 +30,8 @@ export default defineComponent({
     }
   },
   components: {
-    MainDashboard
+    MainDashboard,
+    OrderDashboard
   }
 })
 </script>
